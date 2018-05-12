d3fa81e1f9d821124e1e5956990535d38aca30bc
shureek <poliakovaleek@gmail.com>
2018-05-13 00:49:42 +0300

    *ProgressBAr

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -1,6 +1,8 @@
 package gui;
 
 import actions.AddReports;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,7 +14,6 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import settings.Settings;
-import javafx.concurrent.Task;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,10 +26,7 @@ import static constants.StringsConstant.CHOSE_REPORTS_FOLDER;
 public class Controller {
     @FXML
     private TabPane tabPane = new TabPane();
-
     private  Settings settings = new Settings();
-    private  TabModel tabModel = new TabModel();
-
 
     @FXML
     public void onCancelClickMethod() {
@@ -45,7 +43,6 @@ public class Controller {
         AddReports addReports = new AddReports(settings, reportsFolder);
         new Thread (addReports).start();
         showProgressBarDialog(addReports);
-   //     addReports.addReports(reportsFolder);
         }
 
     @FXML
@@ -59,15 +56,20 @@ public class Controller {
 
     @FXML
     public void onRefreshClickMethod(){
-        int i=0;
-        while (i<tabPane.getTabs().size()){
-            String tabId = tabPane.getTabs().get(i).getId();
-            tabPane.getTabs().get(i).setContent(tabModel.getController().Refresh(tabId));
-            i++;
-        }
 
-        new Thread(getTask()).start();
-    }
+        Task task = getTaskFillTab();
+        new Thread(new Task() {
+            @Override
+            public Object call() {
+                showProgressBarDialog(task);
+                return null;
+            }
+        }).start();
+       Thread bgThread = new Thread(task);
+       bgThread.setDaemon(true);
+       bgThread.start();
+       showProgressBarDialog(task);
+       }
 
     @FXML
     private void initialize(){
@@ -114,14 +116,14 @@ public class Controller {
         progressBarController.setTask(task);
         Stage progressBarStage = new Stage();
         progressBarStage.setResizable(false);
+        pane.setStyle(" -fx-border-color:c3c3c3; -fx-border-width: 2pt;");
         progressBarStage.initStyle(StageStyle.UNDECORATED);
        // progressBarStage.setTitle("rogressBar");
         progressBarStage.initModality(Modality.APPLICATION_MODAL);
         Scene scene = new Scene(pane);
         progressBarStage.setScene(scene);
-
     //    AddTabController controller = showProgressBar.getController();
-        progressBarStage.showAndWait();
+        progressBarStage.show();
 
     }
      catch (IOException e) {
@@ -129,14 +131,47 @@ public class Controller {
     }
     }
 
-    Task getTask(){
-        return new  Task(){
+    public Task getTaskFillTab(){
+        return new Task(){
             @Override
             protected Object call() throws Exception {
+                int i=0;
+                int size = tabPane.getTabs().size();
+                while (i<size){
+                    updateProgress(i, size);
+                    Thread.sleep(1000);
+                    Tab tab =  tabPane.getTabs().get(i);
+                    fillTab(tab);
+                    i++;
 
+                }
                 return null;
             }
         };
     }
+
+    private void fillTab(Tab targetTab ){
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                TabModel tabModel = new TabModel();
+                targetTab.setContent(tabModel.getController().Refresh(targetTab.getId()));
+                ;
+
+            }
+        });
+
+    }
+/*
+    public void onRefreshClickMethod(){
+        int i=0;
+        while (i<tabPane.getTabs().size()){
+            String tabId = tabPane.getTabs().get(i).getId();
+            tabPane.getTabs().get(i).setContent(tabModel.getController().Refresh(tabId));
+            i++;
+        }
+
+        new Thread(getTask()).start();
+    }*/
 
 }
