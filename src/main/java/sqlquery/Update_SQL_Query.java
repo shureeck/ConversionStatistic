92e8fa8914a59f5e0359569e5c4433e9a17d736d
shureek <poliakovaleek@gmail.com>
2018-05-19 21:44:05 +0300

    *Build SQL query was updated

--- src/main/java/sqlquery/Update_SQL_Query.java
+++ src/main/java/sqlquery/Update_SQL_Query.java
@@ -28,87 +28,17 @@ public class Update_SQL_Query {
 
     private ArrayList<String> buildApplyStatement(Report report){
         ArrayList<String> result = new ArrayList<>();
-        int i=0;
-        int size=report.getGeneralStatistic().size();
-        while (i<size) {
-
-            String values[] ={String.valueOf(report.getBuildNumber()), report.getGeneralStatistic().get(i).getName(),
-                    String.valueOf(report.getGeneralStatistic().get(i).getCount()), report.getPair(), report.getFolder()};
-            String valuesGeneralStatistic = String.format(VALUES_STATISTIC, values);
-
-            String args []={TABLE_APPLY_GENERAL_STATISTIC, FIELDS_APPLY_GENERAL_STATISTIC, valuesGeneralStatistic};
-            result.add(String.format(REPLACE_INTO, args));
-            i++;
-        }
-
-       i=0;
-         size=report.getStatisticByCategories().size();
-        while (i<size) {
-            String values[]={String.valueOf(report.getBuildNumber()), report.getStatisticByCategories().get(i).getName(),
-                    String.valueOf(report.getStatisticByCategories().get(i).getPassed()),
-                    String.valueOf(report.getStatisticByCategories().get(i).getFailed()), report.getPair(),report.getFolder()};
-            String valuesStatisticByCategories = String.format(VALUES_STATISTIC_BY_CATEGORIES, values);
-
-            String args[] = {TABLE_APPLY_BY_CATEGORIES, FIELDS_APPLY_BY_CATEGORIES, valuesStatisticByCategories};
-            result.add(String. format(REPLACE_INTO, args));
-            i++;
-        }
-
-        i=0;
-        size=report.getStatisticBySource().size();
-        while (i<size) {
-            String values[] ={String.valueOf(report.getBuildNumber()),report.getStatisticBySource().get(i).getName(),
-                    String.valueOf(report.getStatisticBySource().get(i).getCount()), report.getPair(), report.getFolder()};
-            String valuesStatisticBySource =String.format(VALUES_STATISTIC , values);
-
-            String arggs[]= {TABLE_STATISTIC_BY_SOURCE, FIELDS_STATISTIC_BY_SOURCE, valuesStatisticBySource};
-            result.add(String.format(REPLACE_INTO, arggs));
-            i++;
-        }
-
-        i =0;
-        size = report.getFailedObjects().size();
-        while (size>i){
-            String category = report.getFailedObjects().get(i).getCategory();
-            String objectName = report.getFailedObjects().get(i).getName();
-            String testlistNumber =String.valueOf(report.getFailedObjects().get(i).getTestListNumber());
-            String errorReport = report.getFailedObjects().get(i).getReport();
-            String [] values ={testlistNumber, category, objectName, String.valueOf(report.getBuildNumber()), report.getFolder(), report.getPair(), FALSE, errorReport};
-            String valuesFaileObjects = String.format(VALUES_FAILED_OBJECTS, values);
-
-            String[] args ={APPLY_FAILED_OBJECTS, FIELDS_APPLY_FAILED_OBJECTS, valuesFaileObjects};
-            result.add(String.format(REPLACE_INTO, args));
-            i++;
-        }
-        return result;
+       result.addAll(getGeneralStatisticQuery(report, TABLE_APPLY_GENERAL_STATISTIC));
+       result.addAll(getStatisticByCategoriesQuery(report, TABLE_APPLY_BY_CATEGORIES));
+       result.addAll(getStatisticBySourceQuery(report, TABLE_STATISTIC_BY_SOURCE));
+       result.addAll(getFailedObjectQuery(report, APPLY_FAILED_OBJECTS));
+       return result;
     }
 
     private ArrayList<String> buildConversionStatement(Report report){
         ArrayList<String> result = new ArrayList<>();
-        int i=0;
-        int size=report.getGeneralStatistic().size();
-        while (i<size) {
-            String values[] = {String.valueOf(report.getBuildNumber()), report.getGeneralStatistic().get(i).getName(),
-                    String.valueOf(report.getGeneralStatistic().get(i).getCount()), report.getPair(), report.getFolder()};
-            String valuesGeneralStatistic =  String.format(VALUES_STATISTIC, values);
-
-            String args[] ={TABLE_CONVERSION_GENERAL_STATISTIC,FIELDS_APPLY_GENERAL_STATISTIC, valuesGeneralStatistic};
-            result.add(String.format(REPLACE_INTO, args));
-            i++;
-        }
-
-        i=0;
-        size=report.getStatisticByCategories().size();
-        while (i<size) {
-            String values[] ={String.valueOf(report.getBuildNumber()), report.getStatisticByCategories().get(i).getName(),
-                    String.valueOf(report.getStatisticByCategories().get(i).getPassed()),
-                    String.valueOf(report.getStatisticByCategories().get(i).getFailed()), report.getPair(), report.getFolder()};
-            String valuesStatisticByCategories = String.format(VALUES_STATISTIC_BY_CATEGORIES, values);
-
-            String args[] = {TABLE_CONVERSION_BY_CATEGORIES, FIELDS_APPLY_BY_CATEGORIES, valuesStatisticByCategories};
-            result.add(String.format(REPLACE_INTO, args));
-            i++;
-        }
+        result.addAll(getGeneralStatisticQuery(report,TABLE_CONVERSION_GENERAL_STATISTIC));
+        result.addAll(getStatisticByCategoriesQuery(report, TABLE_CONVERSION_BY_CATEGORIES));
         return result;
     }
 
@@ -130,18 +60,7 @@ public class Update_SQL_Query {
 
     private ArrayList<String> buildActionItemsStatement(Report report){
         ArrayList<String> result = new ArrayList<>();
-        int i=0;
-        int size=report.getGeneralStatistic().size();
-        String valuesGeneralStatistic="";
-        while (i<size) {
-            String values[] ={String.valueOf(report.getBuildNumber()), report.getGeneralStatistic().get(i).getName(),
-                    String.valueOf(report.getGeneralStatistic().get(i).getCount()), report.getPair(), report.getFolder()};
-            valuesGeneralStatistic = String.format( VALUES_STATISTIC, values);
-
-            String args[] ={TABLE_ACTION_ITEMS_GENERAL_STATISTIC, FIELDS_APPLY_GENERAL_STATISTIC, valuesGeneralStatistic};
-            result.add(String.format(REPLACE_INTO, args));
-            i++;
-        }
+        result.addAll(getGeneralStatisticQuery(report, TABLE_ACTION_ITEMS_GENERAL_STATISTIC ));
     return result;
     }
 
@@ -149,6 +68,75 @@ public class Update_SQL_Query {
         String callStetment = String.format(CALL_CHANGE_TO_TRUE, APPLY_FAILED_OBJECTS, report.getFolder() );
         return callStetment;
 
+    }
+
+   private ArrayList<String> getGeneralStatisticQuery(Report report, String tableName) {
+        ArrayList<String> generalStatisticsQuery = new ArrayList<>();
+        int i=0;
+        int size=report.getGeneralStatistic().size();
+        while (i<size) {
+
+            String values[] ={String.valueOf(report.getBuildNumber()), report.getGeneralStatistic().get(i).getName(),
+                    String.valueOf(report.getGeneralStatistic().get(i).getCount()), report.getPair(), report.getFolder()};
+            String valuesGeneralStatistic = String.format(VALUES_STATISTIC, values);
+
+            String args []={tableName, FIELDS_APPLY_GENERAL_STATISTIC, valuesGeneralStatistic};
+            generalStatisticsQuery.add(String.format(REPLACE_INTO, args));
+            i++;
+        }
+        return generalStatisticsQuery;
+    }
+
+    private ArrayList<String> getStatisticByCategoriesQuery(Report report, String tableName){
+        ArrayList <String> StatisticByCategories = new ArrayList<>();
+        int i=0;
+        int size=report.getStatisticByCategories().size();
+        while (i<size) {
+            String values[]={String.valueOf(report.getBuildNumber()), report.getStatisticByCategories().get(i).getName(),
+                    String.valueOf(report.getStatisticByCategories().get(i).getPassed()),
+                    String.valueOf(report.getStatisticByCategories().get(i).getFailed()), report.getPair(),report.getFolder()};
+            String valuesStatisticByCategories = String.format(VALUES_STATISTIC_BY_CATEGORIES, values);
+
+            String args[] = {tableName, FIELDS_APPLY_BY_CATEGORIES, valuesStatisticByCategories};
+            StatisticByCategories.add(String. format(REPLACE_INTO, args));
+            i++;
+        }
+        return StatisticByCategories;
+    }
+
+    private ArrayList<String> getStatisticBySourceQuery(Report report, String tableName){
+        ArrayList <String> statisticBySource = new ArrayList<>();
+        int i=0;
+        int size=report.getStatisticBySource().size();
+        while (i<size) {
+            String values[] ={String.valueOf(report.getBuildNumber()),report.getStatisticBySource().get(i).getName(),
+                    String.valueOf(report.getStatisticBySource().get(i).getCount()), report.getPair(), report.getFolder()};
+            String valuesStatisticBySource =String.format(VALUES_STATISTIC , values);
+
+            String arggs[]= {tableName, FIELDS_STATISTIC_BY_SOURCE, valuesStatisticBySource};
+            statisticBySource.add(String.format(REPLACE_INTO, arggs));
+            i++;
+        }
+        return statisticBySource;
+    }
+
+    private ArrayList<String> getFailedObjectQuery(Report report, String tableName){
+        ArrayList<String> failedObjects = new ArrayList<>();
+        int i =0;
+        int size = report.getFailedObjects().size();
+        while (size>i){
+            String category = report.getFailedObjects().get(i).getCategory();
+            String objectName = report.getFailedObjects().get(i).getName();
+            String testlistNumber =String.valueOf(report.getFailedObjects().get(i).getTestListNumber());
+            String errorReport = report.getFailedObjects().get(i).getReport();
+            String [] values ={testlistNumber, category, objectName, String.valueOf(report.getBuildNumber()), report.getFolder(), report.getPair(), FALSE, errorReport};
+            String valuesFaileObjects = String.format(VALUES_FAILED_OBJECTS, values);
+
+            String[] args ={tableName, FIELDS_APPLY_FAILED_OBJECTS, valuesFaileObjects};
+            failedObjects.add(String.format(REPLACE_INTO, args));
+            i++;
+        }
+        return failedObjects;
     }
 
     public ArrayList<String> getSqlStatement() {
